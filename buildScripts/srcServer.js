4a6e87c786559fda41434a11f2ec64f025e0d131
unknown <bubbastinx@gmail.com>
2019-07-17 16:47:31 -0500

    added babel, .editorconfig and other starting scripts

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+let app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+app.listen(port, err => {
+	if (err) {
+		console.log(err);
+	} else {
+		open('http://localhost:' + port);
+	}
+});
