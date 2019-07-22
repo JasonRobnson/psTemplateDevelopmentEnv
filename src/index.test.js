bae7415b7d5cdbe486b508fae8fdba2cd808db55
unknown <bubbastinx@gmail.com>
2019-07-22 09:14:48 -0500

    added test script to package.json

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,23 @@
 import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
 	it('should pass', () => {
 		expect(true).to.be.true;
+	});
+});
+
+describe('index.html', () => {
+	it('should say hello', done => {
+		const index = fs.readFileSync('./src/index.html', 'utf-8');
+
+		// can also use an Array of Javascript files to load into JSDOM if your FE code uses fetch then need to use isomorphic fetch instead because it won't be available in the node env.
+		jsdom.env(index, (err, window) => {
+			const h1 = window.document.getElementsByTagName('h1')[0];
+			expect(h1.innerHTML).to.equal('Hello World');
+			done();
+			window.close();
+		});
 	});
 });
